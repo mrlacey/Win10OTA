428f6b891fe1c9c49caba8a72fd0f08f2d1b6115
Matt Lacey <github@mrlacey.co.uk>
2015-09-24 20:52:13 +0100

    some stuff

---
+++ Demo/RunWithCustomisationsEverywhere/SecondPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RunWithCustomisationsEverywhere
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondPage : Page
+    {
+        public SecondPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.Visible;
+
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += (s, args) => {
+                if (Frame.CanGoBack)
+                {
+                    this.Frame.GoBack();
+                    args.Handled = true;
+                }
+            };
+        }
+        
+    }
+}
