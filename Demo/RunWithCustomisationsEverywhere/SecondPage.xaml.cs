c882c8986136894c0158b36ba971e9c51b47dcbc
Matt Lacey <github@mrlacey.co.uk>
2015-09-25 01:08:24 +0100

    Added Xam Projects

--- Demo/RunWithCustomisationsEverywhere/SecondPage.xaml.cs
+++ Demo/RunWithCustomisationsEverywhere/SecondPage.xaml.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace RunWithCustomisationsEverywhere
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SecondPage : Page
     {
         public SecondPage()
@@ -31,9 +15,10 @@ namespace RunWithCustomisationsEverywhere
         {
             base.OnNavigatedTo(e);
 
-            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility
+                = AppViewBackButtonVisibility.Visible;
 
-            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += (s, args) => {
+            SystemNavigationManager.GetForCurrentView().BackRequested += (s, args) => {
                 if (Frame.CanGoBack)
                 {
                     this.Frame.GoBack();
