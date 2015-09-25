cafc953449bf5d7838457018d518e6b749deb56c
Matt Lacey <github@mrlacey.co.uk>
2015-09-25 02:42:44 +0100

    all complete + apiinformation check

---
+++ Demo/RunWithCustomisationsEverywhere/MyLogger.cs
@@ -0,0 +1,12 @@
+using Windows.UI.Xaml.Controls;
+
+namespace RunWithCustomisationsEverywhere
+{
+    internal class MyLogger
+    {
+        public static void Log(Page page, bool printingSupported)
+        {
+            // do something with this info
+        }
+    }
+}
